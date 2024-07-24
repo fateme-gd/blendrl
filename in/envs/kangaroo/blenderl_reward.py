dcef8e78da5c3e142d97847efc1d7c9831b3c115
hkrsnd <microapple56@gmail.com>
2024-07-24 14:46:29 +0200

    update kangaroo reward

--- in/envs/kangaroo/blenderl_reward.py
+++ in/envs/kangaroo/blenderl_reward.py
@@ -9,9 +9,11 @@ def reward_function(self, game_reward) -> float:
             break
 
     # got reawrd and previous step was on the top platform -> reached the child
-    if game_reward == 1.0 and player.prev_y == 4:
+    #if game_reward == 1.0 and player.prev_y == 4:
+    #    reward = 100.0
+    if player.y == 4 and player.prev_y != 4:
         reward = 100.0
-    elif game_reward == 1.0:
+    elif game_reward == 1.0 and player.prev_y != 4:
         reward = game_reward
     else:
         reward = 0.0

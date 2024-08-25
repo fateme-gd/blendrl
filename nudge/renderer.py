6b7703ddbf83a8bcd4bd0bd46400588d9b65073e
hkrsnd <microapple56@gmail.com>
2024-08-25 16:32:51 +0200

    update play seaquest

--- nudge/renderer.py
+++ nudge/renderer.py
@@ -293,7 +293,7 @@ class Renderer:
         # nsfr = self.nsfr_reasoner
         nsfr = self.model.actor.logic_actor
         pred_vals = {pred: nsfr.get_predicate_valuation(pred, initial_valuation=False) for pred in nsfr.prednames}
-        print(self.model.actor.logic_action_probs)
+        # print(self.model.actor.logic_action_probs)
         for i, (pred, val) in enumerate(pred_vals.items()):
             i += 2
             # Render cell background

0a70ade251bea8d135e6c0ae469d55e25579fb13
hkrsnd <microapple56@gmail.com>
2025-01-24 15:54:26 +0100

    update: separate neumann dependency

--- play_gui.py
+++ play_gui.py
@@ -17,9 +17,9 @@ import tyro
 
 def main(
     env_name: str = "seaquest",
-    agent_path: str = "out/runs/kangaroo_softmax_lr_0.00025_llr_0.00025_blr_0.00025_gamma_0.99_bentcoef_0.0_numenvs_60_steps_128_pretrained_False_joint_True_20",
-    fps: int = 5,
-    seed: int = 0
+    agent_path: str = "models/kangaroo_demo",
+    seed: int = 0,
+    fps: int = 5
     ) -> None:
     renderer = Renderer(\
         agent_path=agent_path,
